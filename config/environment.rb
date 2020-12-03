5774173f1c164383a5a23f89e4a9aa0b42cae15e
talryan <nataliamryan@gmail.com>
2020-12-03 12:07:35 -0800

    commit tree c5ba56f707d86bc712ddbf77f516143ede9d70dd

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,7 @@
 ENV['SINATRA_ENV'] ||= "development"
+configure :development do
+    set :database, 'sqlite3:db/database.db'
+  end 
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
